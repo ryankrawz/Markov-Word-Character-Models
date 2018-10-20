3f44240df4e98540d4bd1e9e47f90b6a2e526689
Robert Muller <robert.muller2@gmail.com>
2018-10-20 10:28:46 -0400

    initial

---
+++ src/Model.java
@@ -0,0 +1,11 @@
+/* file: Model.java
+   author: Bob Muller
+
+   CSCI 1102 Computer Science 2
+
+   The API for the Model ADT, a part of an implementation
+   of C. Shannon's n-gram algorithm for modeling English text.
+*/
+public interface Model {
+  // YOUR CODE HERE
+}
---
+++ src/ModelC.java
@@ -0,0 +1,17 @@
+/* file: ModelC.java
+   author: Bob Muller
+
+   CSCI 1102 Computer Science 2
+
+   This is an implementation of the Model ADT, a part of an
+   implementation of C. Shannon's n-gram algorithm for
+   modeling English text.
+*/
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+public class ModelC implements Model {
+  // YOUR CODE HERE
+}
