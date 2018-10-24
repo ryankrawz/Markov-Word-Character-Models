eb8b09039fa0a62389438d1d602497bc0c436519
Ryan Krawczyk <ryankrawczyk@ryans-macbook.bc.edu>
2018-10-24 18:54:27 -0400

    refactoring generateMap()

--- src/WordModelC.java
+++ src/WordModelC.java
@@ -10,6 +10,7 @@
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Set;
@@ -20,41 +21,70 @@ import java.lang.Math;
 public class WordModelC implements Model {
 
     private String input, output;
+    private int degree;
     private Map<String, List<String>> map;
 
-    public WordModelC(String input) {
+    public WordModelC(String input, int degree) {
         this.input = input;
+        this.degree = degree;
         this.output = "";
-        this.map = generateMap(input);
+        this.map = generateMap(input, degree);
     }
 
-    private static Map generateMap(String input) {
-        String[] wordArray = input.split(" ");
+    private static String[] subArray(String[] arr, int start, int end) {
+        return Arrays.asList(arr).subList(start, end).toArray(new String[0]);
+    }
+
+    private static Map generateMap(String input, int degree) {
         Map<String, List<String>> map = new HashMap<String, List<String>>();
-        for (int i = 0; i < wordArray.length - 1; i++) {
-            String key = wordArray[i];
+        String[] wordArray = input.split(" ");
+        for (int i = 0; i < wordArray.length - degree; ++i) {
+            StringBuilder key = new StringBuilder(wordArray[i]);
+            for (int j = i + 1; j < i + degree; ++j) {
+                key.append(" ").append(words[j]);
+            }
+            String value = (i + degree < wordArray.length) ? wordArray[i + degree] : "";
             List<String> valueList;
-            if (!map.containsKey(key))              valueList = new ArrayList<String>();
-            else                                    valueList = map.get(key);
-            valueList.add(wordArray[i + 1]);
-            if (i == wordArray.length - 2)          valueList.add(Main.SENTINEL.toString());
-            map.put(key, valueList);
+            if (!map.containsKey(key.toString())) {
+                valueList = new List<String>();
+            } else {
+                valueList = map.get(key.toString());
+            }
+            valueList.add(value);
+            map.put(key.toString(), valueList);
         }
         return map;
+
+        /* for (int i = 0; i < wordArray.length; i++) {
+            String[] key = new String[1];
+            if (i == 0)                    key[0] = "";
+            else if (i < degree)           key = subArray(wordArray, 0, i);
+            else                           key = subArray(wordArray, i - degree, i);
+            List<String> valueList;
+            if (!map.containsKey(key))     valueList = new ArrayList<String>();
+            else                           valueList = map.get(key);
+            if (i == input.length())       valueList.add(Main.SENTINEL.toString());
+            else                           valueList.add(wordArray[i]);
+            map.put(key, valueList);
+        }
+        return map; */
     }
 
     public String generateOutput() {
-        String key = input.split(" ")[0];
-        output += key;
-        int i = -1;
+        String[] key = new String[1];
+        if (output.equals(""))             key[0] = "";
+        else                               key = output.split(" ");
         while (map.containsKey(key)) {
             List<String> valueList = map.get(key);
             int randIndex = (int) Math.floor(Math.random() * valueList.size());
             String randWord = valueList.get(randIndex);
             if (randWord.equals(Main.SENTINEL.toString())) break;
             output += " " + randWord;
-            i++;
-            key = output.split(" ")[i];
+            String[] outputArray = output.split(" ");
+            if (outputArray.length - degree < 0)
+                key = subArray(outputArray, 0, outputArray.length);
+            else
+                key = subArray(outputArray, outputArray.length - degree, outputArray.length);
         }
         return output;
     }
@@ -69,8 +99,8 @@ public class WordModelC implements Model {
     }
 
     public static void main(String[] args) {
-        String inputText = "THIS IS REALLY AMAZING. I WANT TO THANK CONGRESSMAN ALAN WEST. HE IS AN AMAZING GUY. I HAVE BEEN A SUPPORTER OF HIS. HE IS TOUGH, SMART, AND A REAL PATRIOT. ALSO, RICK SCOTT. HE IS DOING A GOOD JOB. IT IS NOT EASY. HE IS DOING A HELLUVA JOB. MY SECOND HOME IS RIGHT DOWN THE ROAD IN YOUR COMPETITIVE COMMUNITY KNOWN AS PALM BEACH. I LOVE FLORIDA. I WOULD LIKE TO THANK THE SOUTH FLORIDA TEA PARTY FOR THE OPPORTUNITY TO ADDRESS THIS GROUP OF HARD-WORKING, INCREDIBLE PEOPLE. IT IS MY GREAT HONOR, BELIEVE ME. [APPLAUSE] OVER THE LAST SIX MONTHS SINCE I STARTED THINKING ABOUT THIS, I HAVE BEEN ASKED SO MUCH ABOUT THE TEA PARTY BY REPORTERS AND A LOT OF DIFFERENT FOLKS. I HAVE COME UP WITH A TRUTHFUL BUT STANDARD ANSWER. THEY ARE GREAT BECAUSE THEY MADE WASHINGTON START THINKING, BOTH DEMOCRATS AND REPUBLICANS.  THEY MADE WASHINGTON START THINKING. I WANT TO THANK YOU ALL. IT IS FANTASTIC. WHEN I WAS ASKED TO DO THIS SPEECH TODAY BY A FRIEND OF MINE, HE SAID IT WOULD BE IN AN AUDITORIUM WITH 250 PEOPLE. WHAT HAPPENED? [CHEERS AND APPLAUSE] WITH ALL OF THE WIND, AT LEAST YOU KNOW IT IS MY REAL HAIR. THE UNITED STATES HAS BECOME THE LAUGHING STOCK AND A WHIPPING POST FOR THE REST OF THE WORLD WHETHER WE LIKE IT OR NOT. WE DO NOT LIKE IT.";
-        Model model = new WordModelC(inputText);
+        String inputText = "Hello my name is Ryan and I like computer science.";
+        Model model = new WordModelC(inputText, 1);
         model.showMap();
         System.out.format("%s%n", model.generateOutput());
     }
